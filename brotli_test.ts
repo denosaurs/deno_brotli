e6850d11953381df0f87faff52c916eb4a80e642
Qu4k <pippo.reds@gmail.com>
2020-05-28 22:45:51 +0200

    test: add unit test

---
+++ brotli_test.ts
@@ -0,0 +1,34 @@
+// Copyright 2020-present the denosaurs team. All rights reserved. MIT license.
+
+import { compress, decompress } from "./mod.ts";
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+
+function encode(input: string): Uint8Array {
+  return new TextEncoder().encode(input);
+}
+
+Deno.test({
+  name: "compress",
+  fn: () => {
+    // empty
+    assertEquals(compress(encode("")), [59]);
+    // 'X' x64 times
+    assertEquals(
+      compress(encode("X".repeat(64))),
+      [27, 63, 0, 0, 36, 176, 226, 153, 64, 18],
+    );
+  },
+});
+
+Deno.test({
+  name: "decompress",
+  fn: () => {
+    // empty
+    assertEquals(decompress(Uint8Array.from([59])), []);
+    // 'X' x64 times
+    assertEquals(
+      decompress(Uint8Array.from([27, 63, 0, 0, 36, 176, 226, 153, 64, 18])),
+      encode("X".repeat(64)),
+    );
+  },
+});
--- src/lib.rs
+++ src/lib.rs
@@ -27,3 +27,31 @@ pub fn decompress(input: &[u8], buffer_size: usize) -> Vec<u8> {
     }
     output
 }
+
+#[test]
+pub fn compress_test() {
+    const BUFFER_SIZE: usize = 4692;
+    const QUALITY: u32 = 6;
+    const WINDOW_SIZE: u32 = 22;
+    assert_eq!(
+        compress("".as_bytes(), BUFFER_SIZE, QUALITY, WINDOW_SIZE),
+        [59]
+    );
+    assert_eq!(
+        compress("X".repeat(64).as_bytes(), BUFFER_SIZE, QUALITY, WINDOW_SIZE),
+        [27, 63, 0, 0, 36, 176, 226, 153, 64, 18]
+    )
+}
+
+#[test]
+pub fn decompress_test() {
+    const BUFFER_SIZE: usize = 4692;
+    assert_eq!(
+        decompress(&[27, 63, 0, 0, 36, 176, 226, 153, 64, 18], BUFFER_SIZE,),
+        "X".repeat(64).as_bytes()
+    );
+    assert_eq!(
+        decompress(&[27, 63, 0, 0, 36, 176, 226, 153, 64, 18], BUFFER_SIZE,),
+        "X".repeat(64).as_bytes()
+    );
+}
