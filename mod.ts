b079b379747eee0210b0c7faf03a32db3127ae27
Qu4k <pippo.reds@gmail.com>
2020-05-28 22:46:06 +0200

    docs: add documentation

--- mod.ts
+++ mod.ts
@@ -1,3 +1,5 @@
+// Copyright 2020-present the denosaurs team. All rights reserved. MIT license.
+
 import init, {
   source,
   compress as wasm_compress,
@@ -6,6 +8,22 @@ import init, {
 
 await init(source);
 
+/**
+ * Compress a byte array.
+ *
+ * ```typescript
+ * import { compress } from "https://deno.land/x/brotli/mod.ts";
+ * const text = new TextEncoder().encode("X".repeat(64));
+ * console.log(text.length);                   // 64 Bytes
+ * console.log(compress(text).length);         // 10 Bytes
+ * ```
+ *
+ * @param input Input data.
+ * @param bufferSize Read buffer size
+ * @param quality Controls the compression-speed vs compression-
+ * density tradeoff. The higher the quality, the slower the compression.
+ * @param lgwin Base 2 logarithm of the sliding window size.
+ */
 export function compress(
   input: Uint8Array,
   bufferSize: number = 4096,
@@ -15,6 +33,19 @@ export function compress(
   return wasm_compress(input, bufferSize, quality, lgwin);
 }
 
+/**
+ * Decompress a byte array.
+ *
+ * ```typescript
+ * import { decompress } from "https://deno.land/x/brotli/mod.ts";
+ * const compressed = Uint8Array.from([ 27, 63, 0, 0, 36, 176, 226, 153, 64, 18 ]);
+ * console.log(compressed.length);             // 10 Bytes
+ * console.log(decompress(compressed).length); // 64 Bytes
+ * ```
+ *
+ * @param input Input data.
+ * @param bufferSize Read buffer size
+ */
 export function decompress(
   input: Uint8Array,
   bufferSize: number = 4096,
