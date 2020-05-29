a7bee89373c1501fa10b2df875e54a17c506ed7d
eliassjogreen <eliassjogreen1@gmail.com>
2020-05-29 02:09:22 +0200

    build: Made the build script a bit more verbose

--- scripts/build.ts
+++ scripts/build.ts
@@ -2,24 +2,24 @@
 
 import Terser from "https://cdn.pika.dev/terser@^4.7.0";
 
+const encoder = new TextEncoder();
+
 const toml = await Deno.stat("Cargo.toml");
 if (!toml.isFile) {
   console.log(
-    "[!] Error: the build script should be executed in the `deno_brotli` root.",
+    `[!] Error: the build script should be executed in the "deno_brotli" root`,
   );
   Deno.exit(1);
 }
 
-const cargo = Deno.run({
-  cmd: ["wasm-pack", "build", "--target", "web", "--release"],
-});
+console.log("[!] building using wasm-pack");
+const pack = Deno.run(
+  { cmd: ["wasm-pack", "build", "--target", "web", "--release"] },
+);
+await pack.status();
 
-await cargo.status();
-
+console.log("[!] inlining wasm in js");
 const wasm = await Deno.readFile("pkg/deno_brotli_bg.wasm");
-const init = await Deno.readTextFile("pkg/deno_brotli.js");
-
-const encoder = new TextEncoder();
 
 let binary = "";
 for (let i = 0, l = wasm.length; i < l; i++) {
@@ -30,12 +30,19 @@ const source = `export const source = Uint8Array.from(atob(\`${
   btoa(binary)
 }\`), c => c.charCodeAt(0));`;
 
-const output = Terser.minify(`${source}\n${init}`);
+const init = await Deno.readTextFile("pkg/deno_brotli.js");
 
+const output = Terser.minify(`${source}\n${init}`);
+console.log(
+  `[!] minified js, size reduction: ${new Blob([(`${source}\n${init}`)]).size -
+    new Blob([output.code]).size} bytes`,
+);
+
+console.log(`[!] writing output to file ("wasm.js")`);
 await Deno.writeFile(
   "wasm.js",
   encoder.encode(`//deno-fmt-ignore-file\n${output.code}`),
 );
 
 const outputFile = await Deno.stat("wasm.js");
-console.log("[!] output file (`wasm.js`) size is:", outputFile.size, "bytes");
+console.log(`[!] output file ("wasm.js") size is: ${outputFile.size} bytes`);
