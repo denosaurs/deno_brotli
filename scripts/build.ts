c8f4423a9e7077ce817224b3e82520087879c88f
eliassjogreen <eliassjogreen1@gmail.com>
2020-05-30 14:50:19 +0200

    build: :zap: Use lz4 compression to reduce wasm size to 1167 KB

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,5 +1,7 @@
 // Copyright 2020-present the denosaurs team. All rights reserved. MIT license.
 
+import { encode } from "https://deno.land/std@0.54.0/encoding/base64.ts";
+import { compress } from "https://deno.land/x/lz4@v0.1.0/mod.ts";
 import Terser from "https://cdn.pika.dev/terser@^4.7.0";
 
 const encoder = new TextEncoder();
@@ -18,21 +20,40 @@ const pack = Deno.run(
 );
 await pack.status();
 
-console.log("[!] inlining wasm in js");
 const wasm = await Deno.readFile("pkg/deno_brotli_bg.wasm");
+const compressed = compress(wasm);
+console.log(
+  `[!] compressed wasm using lz4, size reduction: ${wasm.length -
+    compressed.length} bytes`,
+);
+const encoded = encode(compressed);
+console.log(
+  `[!] encoding wasm using base64, size increase: ${encoded.length -
+    compressed.length} bytes`,
+);
 
-let binary = "";
-for (let i = 0, l = wasm.length; i < l; i++) {
-  binary += String.fromCharCode(wasm[i]);
-}
-
-const source = `export const source = Uint8Array.from(atob(\`${
-  btoa(binary)
-}\`), c => c.charCodeAt(0));`;
+console.log("[!] inlining wasm in js");
+const source = `import * as lz4 from "https://deno.land/x/lz4@v0.1.0/mod.ts";
+                export const source = lz4.decompress(Uint8Array.from(atob("${encoded}"), c => c.charCodeAt(0)));`;
 
 const init = await Deno.readTextFile("pkg/deno_brotli.js");
 
-const output = Terser.minify(`${source}\n${init}`);
+console.log("[!] minifying js");
+
+const output = Terser.minify(`${source}\n${init}`, {
+  mangle: { module: true },
+  output: {
+    preamble: "//deno-fmt-ignore-file",
+  },
+});
+
+if (output.error) {
+  console.log(
+    `[!] Error: ${output.error}`,
+  );
+  Deno.exit(1);
+}
+
 console.log(
   `[!] minified js, size reduction: ${new Blob([(`${source}\n${init}`)]).size -
     new Blob([output.code]).size} bytes`,
@@ -41,7 +62,7 @@ console.log(
 console.log(`[!] writing output to file ("wasm.js")`);
 await Deno.writeFile(
   "wasm.js",
-  encoder.encode(`//deno-fmt-ignore-file\n${output.code}`),
+  encoder.encode(output.code),
 );
 
 const outputFile = await Deno.stat("wasm.js");
