76090cd3bf908aae08c70ddedf88bfaf60675789
eliassjogreen <eliassjogreen1@gmail.com>
2020-06-07 14:00:57 +0200

    build: :arrow_up: Update build script to latest wasabi build script

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,45 +1,79 @@
 // Copyright 2020-present the denosaurs team. All rights reserved. MIT license.
 
-import { encode } from "https://deno.land/std@0.54.0/encoding/base64.ts";
-import { compress } from "https://deno.land/x/lz4@v0.1.0/mod.ts";
+import { encode } from "https://deno.land/std@0.56.0/encoding/base64.ts";
+import { compress } from "https://deno.land/x/lz4@v0.1.1/mod.ts";
 import Terser from "https://cdn.pika.dev/terser@^4.7.0";
+
+const name = "deno_brotli";
 
 const encoder = new TextEncoder();
 
-const toml = await Deno.stat("Cargo.toml");
-if (!toml.isFile) {
-  console.log(
-    `[!] Error: the build script should be executed in the "deno_brotli" root`,
-  );
-  Deno.exit(1);
+async function requires(...executables: string[]) {
+  const where = Deno.build.os === "windows" ? "where" : "which";
+
+  for (const executable of executables) {
+    const process = Deno.run({
+      cmd: [where, executable],
+      stderr: "null",
+      stdin: "null",
+      stdout: "null",
+    });
+
+    if (!(await process.status()).success) {
+      err(`Could not find required build tool ${executable}`);
+    }
+  }
 }
 
-console.log("[!] building using wasm-pack");
-const pack = Deno.run(
-  { cmd: ["wasm-pack", "build", "--target", "web", "--release"] },
-);
-await pack.status();
+async function run(msg: string, cmd: string[]) {
+  log(msg);
 
-const wasm = await Deno.readFile("pkg/deno_brotli_bg.wasm");
+  const process = Deno.run({ cmd });
+
+  if (!(await process.status()).success) {
+    err(`${msg} failed`);
+  }
+}
+
+function log(text: string): void {
+  console.log(`[log] ${text}`);
+}
+
+function err(text: string): never {
+  console.log(`[err] ${text}`);
+  return Deno.exit(1);
+}
+
+await requires("rustup", "rustc", "cargo", "wasm-pack");
+
+if (!(await Deno.stat("Cargo.toml")).isFile) {
+  err(`the build script should be executed in the "${name}" root`);
+}
+
+await run(
+  "building using wasm-pack",
+  ["wasm-pack", "build", "--target", "web", "--release"],
+);
+
+const wasm = await Deno.readFile(`pkg/${name}_bg.wasm`);
 const compressed = compress(wasm);
-console.log(
-  `[!] compressed wasm using lz4, size reduction: ${wasm.length -
+log(
+  `compressed wasm using lz4, size reduction: ${wasm.length -
     compressed.length} bytes`,
 );
 const encoded = encode(compressed);
-console.log(
-  `[!] encoding wasm using base64, size increase: ${encoded.length -
+log(
+  `encoded wasm using base64, size increase: ${encoded.length -
     compressed.length} bytes`,
 );
 
-console.log("[!] inlining wasm in js");
-const source = `import * as lz4 from "https://deno.land/x/lz4@v0.1.0/mod.ts";
+log("inlining wasm in js");
+const source = `import * as lz4 from "https://deno.land/x/lz4@v0.1.1/mod.ts";
                 export const source = lz4.decompress(Uint8Array.from(atob("${encoded}"), c => c.charCodeAt(0)));`;
 
-const init = await Deno.readTextFile("pkg/deno_brotli.js");
+const init = await Deno.readTextFile(`pkg/${name}.js`);
 
-console.log("[!] minifying js");
-
+log("minifying js");
 const output = Terser.minify(`${source}\n${init}`, {
   mangle: { module: true },
   output: {
@@ -48,22 +82,17 @@ const output = Terser.minify(`${source}\n${init}`, {
 });
 
 if (output.error) {
-  console.log(
-    `[!] Error: ${output.error}`,
-  );
-  Deno.exit(1);
+  err(`encountered error when minifying: ${output.error}`);
 }
 
-console.log(
-  `[!] minified js, size reduction: ${new Blob([(`${source}\n${init}`)]).size -
-    new Blob([output.code]).size} bytes`,
-);
+const reduction = new Blob([(`${source}\n${init}`)]).size -
+  new Blob([output.code]).size;
+log(`minified js, size reduction: ${reduction} bytes`);
 
-console.log(`[!] writing output to file ("wasm.js")`);
-await Deno.writeFile(
-  "wasm.js",
-  encoder.encode(output.code),
-);
+log(`writing output to file ("wasm.js")`);
+await Deno.writeFile("wasm.js", encoder.encode(output.code));
 
 const outputFile = await Deno.stat("wasm.js");
-console.log(`[!] output file ("wasm.js") size is: ${outputFile.size} bytes`);
+log(
+  `output file ("wasm.js"), final size is: ${outputFile.size} bytes`,
+);
